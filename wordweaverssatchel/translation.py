6bba97b47d7a1fbb852753b51e021b090dc0dec1
Jake <jakesameullawrence@gmail.com>
2018-12-15 10:49:17 +0000

    Calculating cost and adding new vocab
    - increase translation capabilities to calculate the cost of the phrase
    - Add new form for added vocab
    - add functionality to add vocab

--- wordweaverssatchel/translation.py
+++ wordweaverssatchel/translation.py
@@ -1,5 +1,6 @@
 import flask
 import werkzeug
+import math
 
 cache = []
 
@@ -14,16 +15,29 @@ def moth_to_eng():
 	
 	mothertongue_out = ""
 	english_out = ""
+	base_cost = 0
+	multiplier = 0
+	breakdown = ""
+	
+	print(cache)
 	
 	mothertongue_in = mothertongue_in.split()
 	for i in mothertongue_in:
 		translation = search_by_mothertongue(i)
+		print(cache)
 		if translation is None:
 			return 'No results matched search'
 		mothertongue_out = mothertongue_out + translation[0] + " "
 		english_out = english_out + translation[1] + " "
+		if translation[2] == 'true':
+			multiplier = multiplier + float(translation[3])
+		else:
+			base_cost = base_cost + float(translation[3])
+		breakdown = breakdown + translation[0].strip() + ', ' + translation[1].strip() + ', ' +  translation[2].strip() + ', ' +  translation[3].strip() + '<br/>'
+		
+	cost = base_cost * multiplier
 	
-	return flask.render_template('home.html', english_box_value=english_out, mothertongue_box_value=mothertongue_out)
+	return flask.render_template('home.html', english_box_value=english_out, mothertongue_box_value=mothertongue_out, cost=math.ceil(cost), breakdown=breakdown)
 	
 @bp.route('/eng_to_moth', methods=['POST'])
 def en_to_moth():
@@ -34,6 +48,8 @@ def en_to_moth():
 	
 	mothertongue_out = ""
 	english_out = ""
+	base_cost = 0
+	multiplier = 0
 	
 	english_in = english_in.split()
 	for i in english_in:
@@ -42,8 +58,36 @@ def en_to_moth():
 			return 'No results matched search'
 		mothertongue_out = mothertongue_out + translation[0] + " "
 		english_out = english_out + translation[1] + " "
+		if translation[2] == 'true':
+			multiplier = multiplier + float(translation[3])
+		else:
+			base_cost = base_cost + float(translation[3])
+			
+	cost = base_cost * multiplier
 	
-	return flask.render_template('home.html', mothertongue_box_value=mothertongue_out, english_box_value=english_out)
+	return flask.render_template('home.html', mothertongue_box_value=mothertongue_out, english_box_value=english_out, cost=math.ceil(cost))
+
+@bp.route('/new', methods=['POST'])
+def new_vocabulary():
+	if flask.request.method == 'POST':
+		new_mothertongue = flask.request.form['new_mothertongue']
+		new_english = flask.request.form['new_english']
+		new_arbonym = flask.request.form['new_arbonym']
+		new_cost = flask.request.form['new_cost']
+	else:
+		return "ERROR: Page was not submitted with POST"
+		
+	file = open("resources/mothertongue_dictionary.csv", "a")
+	file.write('\n' 
+			   + new_mothertongue + ','
+			   + new_english + ','
+			   + new_arbonym + ','
+			   + new_cost)
+	file.close()
+	print('clearing cache')
+	clear_cache()
+		
+	return flask.render_template('home.html')
 	
 def search_by_mothertongue(target):		
 	# check if cache is empty, load if it is
@@ -68,8 +112,14 @@ def search_by_english(target):
 	return None
 
 def load_mothertongue_dictionary():
+	global cache
 	# read in data from csv file
-	file = flask.current_app.open_resource("mothertongue_dictionary.csv")
+	file = open("resources/mothertongue_dictionary.csv")
 	for line in file:
-		line = line.decode().split(',')
+		line = line.split(',')
 		cache.append(line)
+		
+def clear_cache():
+	global cache
+	cache = []
+	print('cache cleared!!!')
